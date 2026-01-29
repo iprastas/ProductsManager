206a5af794a7743f1f54b47163f544b7dab98141
Anastasia <Anastasia@DESKTOP-SMPEMPG>
2026-01-29 22:29:00 +0300

    Added api methods

---
+++ IronWaterStudio_Test_Stoianova/Controllers/Api/ProductsApiController.cs
@@ -0,0 +1,60 @@
+﻿using IronWaterStudio_Test_Stoianova.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IronWaterStudio_Test_Stoianova.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ProductsApiController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetListOfProducts(string? name, decimal? minPrice, decimal? maxPrice)
+        {
+            var query = _appDbContext.Products.AsQueryable();
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(x => x.Name.Contains(name));
+            }
+
+            if (minPrice.HasValue)
+            {
+                query = query.Where(x => x.Price >= minPrice.Value);
+            }
+
+            if (maxPrice.HasValue)
+            {
+                query = query.Where(x => x.Price <= maxPrice.Value);
+            }
+
+            var res = await query.Select(x => new
+            { 
+                x.Id,
+                x.Name,
+                x.Price
+            }).ToListAsync();
+
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _appDbContext.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
